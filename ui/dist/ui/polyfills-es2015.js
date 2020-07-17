96e9b4aa411a9a65191d155743d8b5fc9baea885
Jayesh Raghuwanshi <jayesh.raghuwanshi2207@gmail.com>
2020-07-17 16:13:35 -0400

    Updating UI files

--- ui/dist/ui/polyfills-es2015.js
+++ ui/dist/ui/polyfills-es2015.js
@@ -3033,7 +3033,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /home/jayesh/csye6225/Assignment 6/webapp/ui/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /home/jayesh/csye6225/Assignment 8/webapp/ui/src/polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
